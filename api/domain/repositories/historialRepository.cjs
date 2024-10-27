c99d0ed0b537cb1b4fd0369d83daf5d4b029d433
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-10-26 23:13:20 -0500

    feat: :sparkles: Creation of the endpoints for getting the record, updating a record and the logic for insert a record in the collection is completed
    Is posible to have some bugs so its open to changes and improvements.

---
+++ api/domain/repositories/historialRepository.cjs
@@ -0,0 +1,51 @@
+const { ObjectId } = require('mongodb');
+const Historial = require('../models/historialModel.cjs');
+
+module.exports = class HistorialRepository {
+    async getAll() {
+        try {
+            const historial = new Historial();
+            return await historial.findAll();
+        } catch (error) {
+            console.error('Error en getAll:', error);
+            throw new Error(JSON.stringify({ status: 404, message: 'Notas no encontradas.' }));
+        }
+    }
+
+    async getById(id) {
+        try {
+            const historial = new Historial();
+            return await historial.findById(id);
+        } catch (error) {
+            throw new Error(JSON.stringify({ status: 404, message: 'Historial no encontrada.' }));
+        }
+    }
+
+    async save(noteData) {
+        try {
+            const historial = new Historial();
+            return await historial.insert(noteData);
+        } catch (error) {
+            throw new Error(JSON.stringify({ status: 500, message: 'Error guardando historial.' }));
+        }
+    }
+
+    async modify(id, dataUpdate) {
+        try {
+            const historial = new Historial();
+            return await historial.update(id, dataUpdate);
+        } catch (error) {
+            throw new Error(JSON.stringify({ status: 500, message: 'Error actualizando historial.' }));
+        }
+    }
+
+    async remove(id) {
+        try {
+            const historial = new Historial();
+            return await historial.delete(id);
+        } catch (error) {
+            throw new Error(JSON.stringify({ status: 500, message: 'Error eliminando historial.' }));
+        }
+    }
+
+}
